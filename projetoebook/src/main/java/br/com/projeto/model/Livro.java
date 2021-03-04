0be79b620a7b84e03e04977eb922fd922a208909
Sandro0330 <sandro030486@gmail.com>
2021-03-04 15:49:38 -0300

    Adicionando Data de Publicação

--- projetoebook/src/main/java/br/com/projeto/model/Livro.java
+++ projetoebook/src/main/java/br/com/projeto/model/Livro.java
@@ -2,6 +2,7 @@ package br.com.projeto.model;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -10,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
@@ -38,6 +41,9 @@ public class Livro {
 	
 	@Min(50) //número de páginas
 	private Integer numeroPaginas;
+	
+	@Temporal(TemporalType.DATE)
+	private Calendar  dataPublicacao = Calendar.getInstance();
 		
 	@ManyToMany
 	@Size(min = 1)
@@ -88,6 +94,17 @@ public class Livro {
 	public void setNumeroPaginas(Integer numeroPaginas) {
 		this.numeroPaginas = numeroPaginas;
 	}
+	
+
+	public Calendar getDataPublicacao() {
+		return dataPublicacao;
+	}
+
+
+	public void setDataPublicacao(Calendar dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
+	}
+
 
 	@Override
 	public String toString() {
